27ae9492c6dbc95e7eeb061ca0f71dc9d2793853
Bharath Srinivas <cybermaster192@gmail.com>
2020-03-23 02:55:36 +0530

    added unit test for new config

--- config/config_test.go
+++ config/config_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func Test_New(t *testing.T) {
+	err := New("test_token", "aTestOauthToken123")
+	expected := ""
+	if err != nil {
+		t.Errorf("got: %s, want: %s", err.Error(), expected)
+	}
+}
+
 func Test_GetConfig(t *testing.T) {
 	config := GetConfig()
 	expectedType := "config.Config"
