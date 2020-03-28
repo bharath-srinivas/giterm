6072041f9a8d47b894e45534ff1f2d29b2c1eecf
Bharath Srinivas <cybermaster192@gmail.com>
2020-03-28 21:32:15 +0530

    added user stats widget to profile page

--- modules/user.go
+++ modules/user.go
@@ -2,12 +2,15 @@ package modules
 
 import (
 	"fmt"
-
+	"github.com/google/go-github/v29/github"
 	"github.com/rivo/tview"
 
 	"github.com/bharath-srinivas/giterm/config"
 	"github.com/bharath-srinivas/giterm/views"
 )
+
+// user holds the github user information.
+var user *github.User
 
 // User represents a github user.
 type User struct {
@@ -17,37 +20,78 @@ type User struct {
 // UserWidget returns a new instance of user widget.
 func UserWidget(app *tview.Application, config config.Config) *User {
 	widget := views.NewTextView(app, config, true)
-	widget.SetWrap(false).SetTitle(string('\U0001F464') + " [green::b]User")
-	p := &User{TextWidget: widget}
-	go p.Refresh()
-	return p
+	widget.SetWrap(false).
+		SetTitle(string('\U0001F464') + " [green::b]User")
+	u := &User{TextWidget: widget}
+	go u.Refresh()
+	return u
 }
 
 // Refresh refreshes the user widget.
-func (p *User) Refresh() {
-	p.Redraw(p.display)
+func (u *User) Refresh() {
+	u.Redraw(u.display)
 }
 
 // display renders the user data in a text view.
-func (p *User) display() {
-	user, _, err := p.Client.Users.Get(p.Context, "")
+func (u *User) display() {
+	var err error
+	user, _, err = u.Client.Users.Get(u.Context, "")
 	if err != nil {
-		_, _ = fmt.Fprint(p, "[::b]an error occurred while retrieving user data")
+		_, _ = fmt.Fprint(u, "[::b]an error occurred while retrieving user data")
 		return
 	}
 
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Name", user.GetName())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Username", user.GetLogin())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Company", user.GetCompany())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Blog", user.GetBlog())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Location", user.GetLocation())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Email", user.GetEmail())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%s\n", "Bio", user.GetBio())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Public Repos", user.GetPublicRepos())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Public Gists", user.GetPublicGists())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Followers", user.GetFollowers())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Following", user.GetFollowing())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Owned Private Repos", user.GetOwnedPrivateRepos())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Total Private Repos", user.GetTotalPrivateRepos())
-	_, _ = fmt.Fprintf(p, "[gray::b]%s: [white]%d\n", "Private Gists", user.GetPrivateGists())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Name", user.GetName())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Username", user.GetLogin())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Bio", user.GetBio())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Company", user.GetCompany())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Location", user.GetLocation())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Email", user.GetEmail())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%s\n", "Blog", user.GetBlog())
+}
+
+// UserStats represents various stats of a github user.
+type UserStats struct {
+	*views.TextWidget
+}
+
+// UserStatsWidget returns a new instance of user stats widget.
+func UserStatsWidget(app *tview.Application, config config.Config) *UserStats {
+	widget := views.NewTextView(app, config, true)
+	widget.SetWrap(false).
+		SetTitle("[green::b]User stats")
+	u := &UserStats{TextWidget: widget}
+	go u.Refresh()
+	return u
+}
+
+// Refresh refreshes the user stats widget.
+func (u *UserStats) Refresh() {
+	u.Redraw(u.display)
+}
+
+// display renders the user data in a text view.
+func (u *UserStats) display() {
+	if user == nil {
+		_, _ = fmt.Fprint(u, "[::b]an error occurred while retrieving user data")
+		return
+	}
+
+	organizations, _, err := u.Client.Organizations.List(u.Context, "", &github.ListOptions{
+		Page:    1,
+		PerPage: 100,
+	})
+	if err != nil {
+		_, _ = fmt.Fprint(u, "[::b]an error occurred while retrieving user data")
+		return
+	}
+
+	orgCount := len(organizations)
+	totalRepos := user.GetPublicRepos() + user.GetTotalPrivateRepos()
+	totalGists := user.GetPublicGists() + user.GetPrivateGists()
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Repositories", totalRepos)
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Gists", totalGists)
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Organizations", orgCount)
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Followers", user.GetFollowers())
+	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Following", user.GetFollowing())
 }
--- pages/profile.go
+++ pages/profile.go
@@ -10,10 +10,16 @@ import (
 // ProfilePage returns the profile page.
 func ProfilePage(app *tview.Application, config config.Config) *Page {
 	user := modules.UserWidget(app, config)
+	userStats := modules.UserStatsWidget(app, config)
 	contribution := modules.ContributionsWidget(app, config)
 
-	layout := tview.NewFlex().
+	userFlex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
 		AddItem(user, 0, 1, false).
+		AddItem(userStats, 0, 1, false)
+
+	layout := tview.NewFlex().
+		AddItem(userFlex, 0, 1, false).
 		AddItem(contribution, 0, 4, false)
 
 	layout.SetTitle(string('\U0001F642') + " [green::b]Profile").
@@ -23,7 +29,7 @@ func ProfilePage(app *tview.Application, config config.Config) *Page {
 		Name: "Profile",
 		Widgets: &Widgets{
 			Parent:   layout,
-			Children: []tview.Primitive{user, contribution},
+			Children: []tview.Primitive{user, userStats, contribution},
 		},
 	}
 }
