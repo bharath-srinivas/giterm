43ce8eda76cca4f25e699ae048e89bb971e5a88b
Bharath Srinivas <cybermaster192@gmail.com>
2020-04-04 22:32:20 +0530

    added graphql client

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/google/go-github/v30 v30.1.0
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/rivo/tview v0.0.0-20200219210816-cd38d7432498
+	github.com/shurcooL/githubv4 v0.0.0-20191127044304-8f68eb5628d0
+	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.6.2
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
--- views/client.go
+++ views/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/google/go-github/v30/github"
+	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 
 	"github.com/bharath-srinivas/giterm/config"
@@ -14,7 +15,8 @@ var user *github.User
 
 // Client represents the github client.
 type Client struct {
-	Username string
+	Username  string
+	GqlClient *githubv4.Client
 
 	*github.Client
 	context.Context
@@ -24,8 +26,9 @@ type Client struct {
 func NewClient(config config.Config) *Client {
 	ctx := context.Background()
 	client := &Client{
-		Client:  githubClient(ctx, config),
-		Context: ctx,
+		Context:   ctx,
+		Client:    githubV3Client(ctx, config),
+		GqlClient: githubV4Client(ctx, config),
 	}
 	client.getUsername()
 	return client
@@ -36,13 +39,22 @@ func (c *Client) GetUser() *github.User {
 	return user
 }
 
-// githubClient returns a new github client with the provided token and context.
-func githubClient(context context.Context, config config.Config) *github.Client {
+// githubV3Client returns a new github API client with the provided token and context.
+func githubV3Client(context context.Context, config config.Config) *github.Client {
 	tokenSource := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: config.Token},
 	)
 	oauth2Client := oauth2.NewClient(context, tokenSource)
 	return github.NewClient(oauth2Client)
+}
+
+// githubV4Client returns a new github graphql client with the provided token and context.
+func githubV4Client(context context.Context, config config.Config) *githubv4.Client {
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: config.Token},
+	)
+	oauth2Client := oauth2.NewClient(context, tokenSource)
+	return githubv4.NewClient(oauth2Client)
 }
 
 // getUsername returns the username of the current user.
