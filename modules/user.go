8ddcb875a39a5978793d612981095ff2629ce82f
Bharath Srinivas <cybermaster192@gmail.com>
2020-04-11 22:20:49 +0530

    separated user stats widget

--- modules/user.go
+++ modules/user.go
@@ -102,38 +102,3 @@ func formatText(title, text, prefix, suffix string) string {
 	}
 	return title + " " + prefix + text + suffix
 }
-
-// UserStats represents various stats of a github user.
-type UserStats struct {
-	*views.TextWidget
-}
-
-// UserStatsWidget returns a new instance of user stats widget.
-func UserStatsWidget(app *tview.Application, config config.Config) *UserStats {
-	widget := views.NewTextView(app, config, true)
-	widget.SetWrap(false).
-		SetTitle("[green::b]User stats")
-	u := &UserStats{widget}
-	go u.Refresh()
-	return u
-}
-
-// Refresh refreshes the user stats widget.
-func (u *UserStats) Refresh() {
-	u.Redraw(u.display)
-}
-
-// display renders the user stats data in a text view.
-func (u *UserStats) display() {
-	if user == (userQuery{}) {
-		_, _ = fmt.Fprint(u, "[::b]an error occurred while retrieving user data")
-		return
-	}
-
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Repositories", user.Viewer.Repositories.TotalCount)
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Gists", user.Viewer.Gists.TotalCount)
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Organizations", user.Viewer.Organizations.TotalCount)
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Starred", user.Viewer.StarredRepositories.TotalCount)
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Followers", user.Viewer.Followers.TotalCount)
-	_, _ = fmt.Fprintf(u, "[gray::b]%s: [white]%d\n", "Following", user.Viewer.Following.TotalCount)
-}
